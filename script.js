422dc25147bb544db9d20b4710fc87299dd88338
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-09-29 18:29:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const request = new XMLHttpRequest();
+
+request.open('GET','https://restcountries.com/v3.1/name/portugal');
+request.send();
+
+console.log(request.responseText)
+
+request.addEventListener('load', function(){
+  const data = JSON.parse(this.responseText)
+  console.log(data)
+})
