b52736b263343ebe47ea80770c237bee449c0a12
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-09-30 13:08:10 +0100

    extracted data from API

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ request.send();
 console.log(request.responseText)
 
 request.addEventListener('load', function(){
-  const data = JSON.parse(this.responseText)
+  const [data] = JSON.parse(this.responseText)
   console.log(data)
+  const html = `<article class="country">
+    <img class="country__img" src=${data.flags.png}/>
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+      <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+    </div>
+  </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend',html);
+  countriesContainer.style.opacity = 1
 })
